2ca437e00540c735d5d646b2f367e2ff6ce8f9b9
Alexandr Drozd <alex.drozd.fd@gmail.com>
2020-10-23 07:11:16 +0300

    feat: implement brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const openBracket = bracketsConfig.map(item => item[0]);
+  
+    const closeBracket = bracketsConfig.map(item => item[1]);
+
+    const stack = [];
+    const strIn = str.split('');
+ 
+    if (strIn.length % 2 !== 0) {
+        return false;                 
+    }
+    
+    for (let i = 0; i <= strIn.length - 1; i++) {
+        
+        if (openBracket.includes(strIn[i])) {
+            stack.push(strIn[i]);
+        } 
+        if (closeBracket.indexOf(strIn[i]) === openBracket.indexOf(stack[stack.length - 1])) {
+           stack.pop();
+        }
+    }
+    return stack.length === 0;
+    
 }
